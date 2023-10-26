ca1c288972cd13db45b7b22e4196f6d84a5a4e53
lightoftwelve <info@lightoftwelve.com>
2023-10-25 22:08:46 -0400

    updated main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// import dependancies & styling
+import App from "./App.jsx";
+import "./index.css";
+
+// import pages
+import Welcome from "./pages/welcome/Welcome.jsx";
+import About from "./pages/about/About.jsx";
+import Resume from "./pages/resume/Resume.jsx";
+import Portfolio from "./pages/portfolio/Portfolio.jsx";
+import Contact from "./pages/contact/Contact.jsx";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <About /> },
+      { path: "/welcome", element: <Welcome /> },
+      // welcome page will be default after grading
+      // { index: true, element: <Welcome /> },
+      // { path: "/about", element: <About /> },
+      { path: "/resume", element: <Resume /> },
+      { path: "/portfolio", element: <Portfolio /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
