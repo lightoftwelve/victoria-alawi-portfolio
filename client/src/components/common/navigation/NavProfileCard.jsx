612d982360235c217554e02d6c4e3f2980e142f3
lightoftwelve <info@lightoftwelve.com>
2023-10-25 22:13:53 -0400

    added Navigation, NavProfileCard components and local styling

---
+++ client/src/components/common/navigation/NavProfileCard.jsx
@@ -0,0 +1,9 @@
+import styles from "./css/NavProfileCard.module.css";
+
+export default function NavIntro() {
+  return (
+    <div className={styles.card}>
+      <h1 className={styles.title}>Victoria Alawi</h1>
+    </div>
+  );
+}
---
+++ client/src/components/common/navigation/Navigation.jsx
@@ -0,0 +1,109 @@
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import NavIntro from "./NavProfileCard";
+
+export default function Navigation() {
+  const [activePage, setActivePage] = useState("/");
+  const navigate = useNavigate();
+  const currentPage = useLocation().pathname;
+
+  const handleHover = (path) => {
+    if (path !== activePage) {
+      setActivePage(path);
+      navigate(path);
+    }
+  };
+
+  return (
+    <div className="navLinks">
+      <NavIntro />
+
+      <ul className="navBar">
+        <li
+          className={
+            currentPage === "/welcome" ? "navPages active" : "navPages"
+          }
+          onMouseOver={() => handleHover("/welcome")}
+        >
+          <Link to="/welcome">
+            <span className="navIcon">
+              <ion-icon name="person-outline"></ion-icon>
+            </span>
+            <span className="navTitle">Welcome</span>
+          </Link>
+        </li>
+        <li
+          className={currentPage === "/" ? "navPages active" : "navPages"}
+          onMouseOver={() => handleHover("/")}
+        >
+          <Link to="/">
+            <span className="navIcon">
+              <ion-icon name="home-outline"></ion-icon>
+            </span>
+            <span className="navTitle">About Me</span>
+          </Link>
+        </li>
+        {/* Will be switching back to this after grading*/}
+        {/* <li
+          className={currentPage === "/" ? "navPages active" : "navPages"}
+          onMouseOver={() => handleHover("/")}
+        >
+          <Link to="/">
+            <span className="navIcon">
+              <ion-icon name="home-outline"></ion-icon>
+            </span>
+            <span className="navTitle">Home</span>
+          </Link>
+        </li> */}
+        {/* <li
+          className={currentPage === "/about" ? "navPages active" : "navPages"}
+          onMouseOver={() => handleHover("/about")}
+        >
+          <Link to="/About">
+            <span className="navIcon">
+              <ion-icon name="person-outline"></ion-icon>
+            </span>
+            <span className="navTitle">About Me</span>
+          </Link>
+        </li> */}
+        <li
+          className={currentPage === "/resume" ? "navPages active" : "navPages"}
+          onMouseOver={() => handleHover("/resume")}
+        >
+          <Link to="/Resume">
+            <span className="navIcon">
+              <ion-icon name="newspaper-outline"></ion-icon>
+            </span>
+            <span className="navTitle">Resume</span>
+          </Link>
+        </li>
+        <li
+          className={
+            currentPage === "/portfolio" ? "navPages active" : "navPages"
+          }
+          onMouseOver={() => handleHover("/portfolio")}
+        >
+          <Link to="/portfolio">
+            <span className="navIcon">
+              <ion-icon name="git-branch-outline"></ion-icon>
+            </span>
+            <span className="navTitle">Portfolio</span>
+          </Link>
+        </li>
+        <li
+          className={
+            currentPage === "/contact" ? "navPages active" : "navPages"
+          }
+          onMouseOver={() => handleHover("/contact")}
+        >
+          <Link to="/contact">
+            <span className="navIcon">
+              <ion-icon name="paper-plane-outline"></ion-icon>
+            </span>
+            <span className="navTitle">Contact</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
