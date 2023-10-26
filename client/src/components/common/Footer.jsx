cb0c43166bde59018a92b241067182fe238aea59
lightoftwelve <info@lightoftwelve.com>
2023-10-25 22:12:15 -0400

    added Header & Footer components

---
+++ client/src/components/common/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer>
+      <span className="footerCredits">
+        © 2023 Victoria Alawi | All Rights Reserved |
+      </span>
+      <span className="footerConnect">
+        <ion-icon name="logo-linkedin"></ion-icon>
+        <a href="https://www.linkedin.com/in/victoria-alawi-872984250">
+          LinkedIn
+        </a>
+        <ion-icon name="git-branch-outline"></ion-icon>
+        <a href="https://github.com/lightoftwelve">Github</a>
+        <ion-icon name="globe-outline"></ion-icon>
+        <a href="https://www.lightoftwelve.com">Shop</a>
+      </span>
+    </footer>
+  );
+}
