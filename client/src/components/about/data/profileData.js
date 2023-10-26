c0fe475c96c264ac9ca9d554a55487a0b451e7c4
lightoftwelve <info@lightoftwelve.com>
2023-10-25 22:17:20 -0400

    added profile data to profileData.js

---
+++ client/src/components/about/data/profileData.js
@@ -0,0 +1,23 @@
+export const profileData = {
+  name: "Victoria Alawi",
+  image: "/Profile.jpeg",
+  titles: ["Fullstack Developer", "SEO Expert", "E-Commerce Store Owner"],
+  links: [
+    {
+      href: "https://github.com/lightoftwelve",
+      icon: "logo-github",
+    },
+    {
+      href: "https://www.linkedin.com/in/victoria-alawi-872984250/",
+      icon: "logo-linkedin",
+    },
+    {
+      href: "mailto:info@lightoftwelve.com",
+      icon: "mail-outline",
+    },
+    {
+      href: "https://www.lightoftwelve.com",
+      icon: "storefront-outline",
+    },
+  ],
+};
