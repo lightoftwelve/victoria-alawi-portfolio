4e18a78f9d4642c2ff00080697e6165bed012d91
lightoftwelve <info@lightoftwelve.com>
2023-10-25 22:31:37 -0400

    added Resume page

---
+++ client/src/pages/resume/Resume.jsx
@@ -0,0 +1,11 @@
+import ResumeSection from "../../components/resume/ResumeSection";
+import Waves from "../../components/about/Waves";
+
+export default function Resume() {
+  return (
+    <>
+      <ResumeSection />
+      <Waves />
+    </>
+  );
+}
